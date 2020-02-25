anjmao/wireguard-android

commit 34610b674ac6cd9d4b55b932e138f6761fca1b82

Miscellaneous UI changes
* Target JVM 1.8 from Kotlin as well

* Add EdgeToEdge and coreKtx dependency

* Setup EdgeToEdge

* Ensure insets are dispatched to all fragments

* Add fragment dependency

* Replace FrameLayouts with recommended FragmentContainerView

* Cleanup inset dispatch code

* Resolve deprecation warnings

* MainActivity: Fix backstack bug exposed by fragment 1.2.2

We've been relying on implicit backstack changes to handle removing the detail fragment for a while which is now gone so let's do this properly like we should

* Make exclusions dialog fit more items in viewport

* ThemeChangeAwareActivity: Remove drawable cache buster

Shouldn't be needed anymore.

Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/wireguard/android/activity/ThemeChangeAwareActivity.java
@@ -20,38 +20,6 @@ import java.lang.reflect.Field;
 
 public abstract class ThemeChangeAwareActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String TAG = "WireGuard/" + ThemeChangeAwareActivity.class.getSimpleName();
-    private static boolean lastDarkMode;
-    @Nullable private static Resources lastResources;
-
-    private static synchronized void invalidateDrawableCache(final Resources resources, final boolean darkMode) {
-        if (resources == lastResources && darkMode == lastDarkMode)
-            return;
-
-        try {
-            Field f;
-            Object o = resources;
-            try {
-                f = o.getClass().getDeclaredField("mResourcesImpl");
-                f.setAccessible(true);
-                o = f.get(o);
-            } catch (final Exception ignored) {
-            }
-            f = o.getClass().getDeclaredField("mDrawableCache");
-            f.setAccessible(true);
-            o = f.get(o);
-            try {
-                o.getClass().getMethod("onConfigurationChange", int.class).invoke(o, -1);
-            } catch (final Exception ignored) {
-                o.getClass().getMethod("clear").invoke(o);
-            }
-        } catch (final Exception e) {
-            Log.e(TAG, "Failed to flush drawable cache", e);
-        }
-
-        lastResources = resources;
-        lastDarkMode = darkMode;
-    }
-
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
@@ -75,8 +43,6 @@ public abstract class ThemeChangeAwareActivity extends AppCompatActivity impleme
                     sharedPreferences.getBoolean(key, false) ?
                             AppCompatDelegate.MODE_NIGHT_YES :
                             AppCompatDelegate.MODE_NIGHT_NO);
-            invalidateDrawableCache(getResources(), darkMode);
-            recreate();
         }
     }
 }

app/build.gradle
@@ -12,6 +12,11 @@ android {
         sourceCompatibility JavaVersion.VERSION_1_8
         targetCompatibility JavaVersion.VERSION_1_8
     }
+    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).all {
+        kotlinOptions {
+            jvmTarget = '1.8'
+        }
+    }
     compileSdkVersion 29
     dataBinding.enabled true
     defaultConfig {
@@ -71,8 +76,10 @@ ext {
     annotationsVersion = '1.1.0'
     appcompatVersion = '1.1.0'
     cardviewVersion = '1.0.0'
+    coreKtxVersion = '1.2.0'
     coordinatorLayoutVersion = '1.1.3'
     databindingVersion = '3.6.0'
+    fragmentVersion = '1.2.2'
     materialComponentsVersion = '1.1.0'
     jsr305Version = '3.0.2'
     kotlinVersion = '1.3.61'
@@ -90,7 +97,9 @@ dependencies {
     implementation "androidx.annotation:annotation:$annotationsVersion"
     implementation "androidx.appcompat:appcompat:$appcompatVersion"
     implementation "androidx.cardview:cardview:$cardviewVersion"
+    implementation "androidx.core:core-ktx:$coreKtxVersion"
     implementation "androidx.databinding:databinding-runtime:$databindingVersion"
+    implementation "androidx.fragment:fragment:$fragmentVersion"
     implementation "androidx.preference:preference:$preferenceVersion"
     implementation "com.google.android.material:material:$materialComponentsVersion"
     implementation "com.google.code.findbugs:jsr305:$jsr305Version"

app/src/main/java/com/wireguard/android/activity/MainActivity.java
@@ -9,17 +9,25 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentManager.OnBackStackChangedListener;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnApplyWindowInsetsListener;
+import android.view.ViewGroup;
+import android.view.WindowInsets;
 import android.widget.LinearLayout;
 
 import com.wireguard.android.R;
 import com.wireguard.android.fragment.TunnelDetailFragment;
 import com.wireguard.android.fragment.TunnelEditorFragment;
 import com.wireguard.android.model.Tunnel;
+
+import java.util.List;
 
 /**
  * CRUD interface for WireGuard tunnels. This activity serves as the main entry point to the
@@ -42,6 +50,7 @@ public class MainActivity extends BaseActivity
         }
         // Deselect the current tunnel on navigating back from the detail pane to the one-pane list.
         if (!isTwoPaneLayout && backStackEntries == 1) {
+            getSupportFragmentManager().popBackStack();
             setSelectedTunnel(null);
             return;
         }
@@ -68,6 +77,16 @@ public class MainActivity extends BaseActivity
         isTwoPaneLayout = findViewById(R.id.master_detail_wrapper) instanceof LinearLayout;
         getSupportFragmentManager().addOnBackStackChangedListener(this);
         onBackStackChanged();
+        // Dispatch insets on back stack change
+        // This is required to ensure replaced fragments are also able to consume insets
+        findViewById(R.id.master_detail_wrapper).setOnApplyWindowInsetsListener((OnApplyWindowInsetsListener) (v, insets) -> {
+            final FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.addOnBackStackChangedListener(() -> {
+                final List<Fragment> fragments = fragmentManager.getFragments();
+                fragments.get(fragments.size() - 1).requireView().dispatchApplyWindowInsets(insets);
+            });
+            return insets;
+        });
     }
 
     @Override

app/src/main/java/com/wireguard/android/fragment/TunnelDetailFragment.java
@@ -20,6 +20,7 @@ import com.wireguard.android.databinding.TunnelDetailFragmentBinding;
 import com.wireguard.android.databinding.TunnelDetailPeerBinding;
 import com.wireguard.android.model.Tunnel;
 import com.wireguard.android.model.Tunnel.State;
+import com.wireguard.android.ui.EdgeToEdge;
 import com.wireguard.crypto.Key;
 
 import java.util.Timer;
@@ -72,6 +73,8 @@ public class TunnelDetailFragment extends BaseFragment {
         super.onCreateView(inflater, container, savedInstanceState);
         binding = TunnelDetailFragmentBinding.inflate(inflater, container, false);
         binding.executePendingBindings();
+        EdgeToEdge.setUpRoot((ViewGroup) binding.getRoot());
+        EdgeToEdge.setUpScrollingContent((ViewGroup) binding.getRoot(), null);
         return binding.getRoot();
     }
 

app/src/main/java/com/wireguard/android/fragment/TunnelEditorFragment.java
@@ -28,6 +28,7 @@ import com.wireguard.android.databinding.TunnelEditorFragmentBinding;
 import com.wireguard.android.fragment.AppListDialogFragment.AppExclusionListener;
 import com.wireguard.android.model.Tunnel;
 import com.wireguard.android.model.TunnelManager;
+import com.wireguard.android.ui.EdgeToEdge;
 import com.wireguard.android.util.ErrorMessages;
 import com.wireguard.android.viewmodel.ConfigProxy;
 import com.wireguard.config.Config;
@@ -89,6 +90,8 @@ public class TunnelEditorFragment extends BaseFragment implements AppExclusionLi
         super.onCreateView(inflater, container, savedInstanceState);
         binding = TunnelEditorFragmentBinding.inflate(inflater, container, false);
         binding.executePendingBindings();
+        EdgeToEdge.setUpRoot((ViewGroup) binding.getRoot());
+        EdgeToEdge.setUpScrollingContent(binding.mainContainer, null);
         return binding.getRoot();
     }
 
@@ -168,11 +171,10 @@ public class TunnelEditorFragment extends BaseFragment implements AppExclusionLi
     }
 
     public void onRequestSetExcludedApplications(@SuppressWarnings("unused") final View view) {
-        final FragmentManager fragmentManager = getFragmentManager();
-        if (fragmentManager != null && binding != null) {
+        if (binding != null) {
             final ArrayList<String> excludedApps = new ArrayList<>(binding.getConfig().getInterface().getExcludedApplications());
             final AppListDialogFragment fragment = AppListDialogFragment.newInstance(excludedApps, this);
-            fragment.show(fragmentManager, null);
+            fragment.show(getParentFragmentManager(), null);
         }
     }
 

app/src/main/java/com/wireguard/android/fragment/TunnelListFragment.java
@@ -37,6 +37,7 @@ import com.wireguard.android.databinding.ObservableKeyedRecyclerViewAdapter;
 import com.wireguard.android.databinding.TunnelListFragmentBinding;
 import com.wireguard.android.databinding.TunnelListItemBinding;
 import com.wireguard.android.model.Tunnel;
+import com.wireguard.android.ui.EdgeToEdge;
 import com.wireguard.android.util.ErrorMessages;
 import com.wireguard.android.widget.MultiselectableRelativeLayout;
 import com.wireguard.config.BadConfigException;
@@ -78,9 +79,7 @@ public class TunnelListFragment extends BaseFragment {
             Config.parse(new ByteArrayInputStream(configText.getBytes(StandardCharsets.UTF_8)));
 
             // Config text is valid, now create the tunnel…
-            final FragmentManager fragmentManager = getFragmentManager();
-            if (fragmentManager != null)
-                ConfigNamingDialogFragment.newInstance(configText).show(fragmentManager, null);
+            ConfigNamingDialogFragment.newInstance(configText).show(getParentFragmentManager(), null);
         } catch (final BadConfigException | IOException e) {
             onTunnelImportFinished(Collections.emptyList(), Collections.singletonList(e));
         }
@@ -219,9 +218,12 @@ public class TunnelListFragment extends BaseFragment {
         binding.createFab.setOnClickListener(v -> {
             final AddTunnelsSheet bottomSheet = new AddTunnelsSheet();
             bottomSheet.setTargetFragment(this, REQUEST_TARGET_FRAGMENT);
-            bottomSheet.show(requireFragmentManager(), "BOTTOM_SHEET");
+            bottomSheet.show(getParentFragmentManager(), "BOTTOM_SHEET");
         });
         binding.executePendingBindings();
+        EdgeToEdge.setUpRoot((ViewGroup) binding.getRoot());
+        EdgeToEdge.setUpFAB(binding.createFab);
+        EdgeToEdge.setUpScrollingContent(binding.tunnelList, binding.createFab);
         return binding.getRoot();
     }
 

app/src/main/java/com/wireguard/android/ui/EdgeToEdge.kt
@@ -1,0 +1,67 @@
+/*
+ * Copyright © 2017-2020 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+
+/**
+ * A utility for edge-to-edge display. It provides several features needed to make the app
+ * displayed edge-to-edge on Android Q with gestural navigation.
+ */
+
+object EdgeToEdge {
+
+    @JvmStatic
+    fun setUpRoot(root: ViewGroup) {
+        root.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+    @JvmStatic
+    fun setUpScrollingContent(scrollingContent: ViewGroup, fab: ExtendedFloatingActionButton?) {
+        val originalPaddingLeft = scrollingContent.paddingLeft
+        val originalPaddingRight = scrollingContent.paddingRight
+        val originalPaddingBottom = scrollingContent.paddingBottom
+
+        val fabPaddingBottom = fab?.height ?: 0
+
+        val originalMarginTop = scrollingContent.marginTop
+
+        scrollingContent.setOnApplyWindowInsetsListener { _, windowInsets ->
+            scrollingContent.updatePadding(
+                left = originalPaddingLeft + windowInsets.systemWindowInsetLeft,
+                right = originalPaddingRight + windowInsets.systemWindowInsetRight,
+                bottom = originalPaddingBottom + fabPaddingBottom + windowInsets.systemWindowInsetBottom
+            )
+            scrollingContent.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = originalMarginTop + windowInsets.systemWindowInsetTop
+            }
+            windowInsets
+        }
+    }
+
+    @JvmStatic
+    fun setUpFAB(fab: ExtendedFloatingActionButton) {
+        val originalMarginLeft = fab.marginLeft
+        val originalMarginRight = fab.marginRight
+        val originalMarginBottom = fab.marginBottom
+        fab.setOnApplyWindowInsetsListener { _, windowInsets ->
+            fab.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = originalMarginLeft + windowInsets.systemWindowInsetLeft
+                rightMargin = originalMarginRight + windowInsets.systemWindowInsetRight
+                bottomMargin = originalMarginBottom + windowInsets.systemWindowInsetBottom
+            }
+            windowInsets
+        }
+    }
+}

app/src/main/res/layout-sw600dp/main_activity.xml
@@ -10,14 +10,14 @@
     android:showDividers="middle"
     tools:context=".activity.MainActivity">
 
-    <fragment
+    <androidx.fragment.app.FragmentContainerView
         android:name="com.wireguard.android.fragment.TunnelListFragment"
         android:layout_width="0dp"
         android:layout_height="match_parent"
         android:layout_weight="2"
         android:tag="LIST" />
 
-    <FrameLayout
+    <androidx.fragment.app.FragmentContainerView
         android:id="@+id/detail_container"
         android:layout_width="0dp"
         android:layout_height="match_parent"

commit d0fccfa9279872afccd4ca64f77ec40f960f61ac

Remove unused variable
Fixes: 34610b674ac6 ("Miscellaneous UI changes")
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

app/src/main/java/com/wireguard/android/activity/ThemeChangeAwareActivity.java
@@ -38,7 +38,6 @@ public abstract class ThemeChangeAwareActivity extends AppCompatActivity impleme
     @Override
     public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
         if ("dark_theme".equals(key)) {
-            final boolean darkMode = sharedPreferences.getBoolean(key, false);
             AppCompatDelegate.setDefaultNightMode(
                     sharedPreferences.getBoolean(key, false) ?
                             AppCompatDelegate.MODE_NIGHT_YES :